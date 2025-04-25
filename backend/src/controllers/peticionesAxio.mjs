16abe7a79c19c3d40f0488b3ce5dd0218173dbef
Guti29 <danigusan29@gmail.com>
2025-04-25 22:30:55 +0200

    feat: add new controllers and routes, update peticionesAxio.mjs

--- backend/src/controllers/peticionesAxio.mjs
+++ backend/src/controllers/peticionesAxio.mjs
@@ -3,6 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import axios from "axios";
+import { access } from "fs";
 
 // Esta parte calcula el path absoluto del archivo actual
 const __filename = fileURLToPath(import.meta.url);
@@ -12,7 +13,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, '..', '..', 'process.env') });
 
 
-/*export const obtenerAeropuertosPorNombre = async (busqueda) => {
+export const obtenerAeropuertosPorNombre = async (busqueda) => {
   try {
     const response = await axios.get("http://api.aviationstack.com/v1/airports", {
       params: {
@@ -31,5 +32,67 @@ dotenv.config({ path: path.join(__dirname, '..', '..', 'process.env') });
     console.error("Error al obtener aeropuertos:", error.message);
     return [];
   }
+};
+
+//Por si necesitamos buscar un vuelo por numero
+export const buscarVuelo = async (numeroVuelo) => {
+  try{
+    const response = await axios.get("http://api.aviationstack.com/v1/flights", {
+      params: {
+        access_key: API_KEY,
+        flight_iata: numeroVuelo,
+      },
+    });
+
+    return response.data.data;
+  }catch (error) {
+    console.error("Error al buscar vuelo:", error.message);
+    return [];
+  }
+};
+
+export const buscarVuelosPorRuta = async (origen, destino) => {
+  try {
+    const response = await axios.get("http://api.aviationstack.com/v1/flights", {
+      params: {
+        access_key: API_KEY,
+        dep_iata: origen,
+        arr_iata: destino,
+      },
+    });
+
+    return response.data.data
+
+  } catch (error) {
+    console.error("Error al buscar vuelos por ruta:", error.message);
+    return [];
+  }
+};
+
+/*
+export const buscarHotelesSimulados = (ciudad) => {
+  // Por si queremos sustituirlo por una API real de hoteles si queremos mas adelante
+  const hoteles = [
+    { nombre: "Hotel Sol", ciudad, estrellas: 4, precio: 80 },
+    { nombre: "Hotel Luna", ciudad, estrellas: 3, precio: 60 },
+    { nombre: "Hotel Estrella", ciudad, estrellas: 5, precio: 120 },
+  ];
+  return hoteles;
+};
+*/
+
+/*
+// Por si queremos generar un mapa de asientos para reservas, esto me lo ha hecho la IA, le he preguntado como haria un mapa de asientos para reservar
+export const generarMapaDeAsientos = (filas = 10, columnas = 6) => {
+  const mapa = [];
+  for (let f = 1; f <= filas; f++) {
+    for (let c = 0; c < columnas; c++) {
+      mapa.push({
+        asiento: `${f}${String.fromCharCode(65 + c)}`,
+        ocupado: false,
+      });
+    }
+  }
+  return mapa;
 };
 */
