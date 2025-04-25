85afe90b6c926b6edde164e679fdc4410d7c8775
Guti29 <danigusan29@gmail.com>
2025-04-25 14:42:13 +0200

    feat--controllersUpdate-newController

---
+++ backend/src/controllers/reservas.mjs
@@ -0,0 +1,7 @@
+export const crearReserva = (req, res) => {
+    //Aqui hay que guardar el vuelo y los datos del usuario en la base de datos
+};
+
+export const obtenerReservasUsuario = (req, res) => {
+    //Coger las reservas del usuario ha iniciado sesion
+};
