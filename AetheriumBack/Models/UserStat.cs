15f86a9d48ef6155bded278df023d8bc3ebb696f
Guti29 <danigusan29@gmail.com>
2025-05-23 23:11:15 +0200

    fix: refactor model

--- AetheriumBack/Models/UserStat.cs
+++ AetheriumBack/Models/UserStat.cs
@@ -10,4 +10,8 @@ public class UserStat
     public int TimesInFirstClass { get; set; }
     public string MostUsedAirline { get; set; }
     public Decimal TotalSpent { get; set; }
+
+#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de agregar el modificador "required" o declararlo como un valor que acepta valores NULL.
+    private UserStat() { }
+#pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de agregar el modificador "required" o declararlo como un valor que acepta valores NULL.
 }
