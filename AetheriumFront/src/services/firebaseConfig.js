b95ce31a77dd362953e6655fbe3cfdbe14fa8bc1
Alexloranca <alexalonsod@gmail.com>
2025-05-27 11:28:36 +0200

    cambios en firebase, ya funciona

--- AetheriumFront/src/services/firebaseConfig.js
+++ AetheriumFront/src/services/firebaseConfig.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+console.log("API KEY:", import.meta.env.VITE_API_KEY);
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
 };
 
 // Inicializar la app de Firebase
