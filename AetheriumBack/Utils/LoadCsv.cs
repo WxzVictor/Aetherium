87d97083884ad6cae0612c2d5d03010cc51abbdf
Guti29 <danigusan29@gmail.com>
2025-05-28 10:47:49 +0200

    feat: inserts airports

--- AetheriumBack/Utils/LoadCsv.cs
+++ AetheriumBack/Utils/LoadCsv.cs
@@ -1,4 +1,6 @@
-﻿using AetheriumBack.Database;
+﻿using System.Globalization;
+using AetheriumBack.Database;
+using AetheriumBack.Models;
 
 namespace AetheriumBack.Utils;
 
@@ -12,19 +14,34 @@ public static class LoadCsv
         if (!context.Airports.Any())
         {
             string path = Directory.GetCurrentDirectory() + "\\Data";
-            IEnumerable<string> files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\Data");
+            IEnumerable<string> files = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory() + "\\Data"))
+                .Where(f => Path.GetFileName(f).ToLower().Contains("aeropuerto"));
 
             if (files.Any())
             {
                 foreach (string file in files)
                 {
                     using StreamReader reader = new(file);
+
+                    reader.ReadLine();
+
                     while (!reader.EndOfStream)
                     {
                         string? line = reader.ReadLine();
                         string[] values = line.Split(',');
 
-                        //Airport aux = new();
+                        Airport airport = new(
+                            values[0],
+                            values[1],
+                            values[2],
+                            values[3],
+                            float.Parse(values[4], CultureInfo.InvariantCulture),
+                            float.Parse(values[5], CultureInfo.InvariantCulture),
+                            int.Parse(values[6], CultureInfo.InvariantCulture),
+                            values[7]
+                        );
+                        
+                        context.Airports.Add(airport);
                     }
                 }
             }
