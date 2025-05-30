2d246479c91c4fc80d9b1a6b94d210b1ea67a015
Guti29 <danigusan29@gmail.com>
2025-05-30 13:14:15 +0200

    fix: finish configuration database and delete old backend

--- backend/src/rutas/api/airports.mjs
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAllAirports } from '../../controllers/airports.mjs';
-
-const router = express.Router();
-
-router.get('/aeropuertos', getAllAirports);
-
-export default router;
--- backend/src/rutas/api/hoteles.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllHotels, getHotelByCity } from '../../controllers/hoteles.mjs';
-
-const router = express.Router();
-
-router.get('/hoteles', getAllHotels);
-router.get('/hoteles/:ciudad', getHotelByCity);
-
-export default router;
