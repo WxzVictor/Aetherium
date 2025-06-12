02d0e26a2cc6fd1339335fe54fb312b2d4741c1c
WxzVictor <viti_111@hotmail.com>
2025-06-12 14:55:13 +0200

    Update confirmReservation.jsx

--- AetheriumFront/src/pages/confirmReservation.jsx
+++ AetheriumFront/src/pages/confirmReservation.jsx
@@ -25,26 +25,37 @@ const ConfirmReservation = () => {
 
     return (
         <Layout>
-            <div className="resultado-vuelos-container">
-                <h1>Confirmar Reserva</h1>
-                <div className="card">
-                    <div className="info">
-                        <strong>{vuelo.airlineName}</strong>
-                        <div>
-                            {vuelo.departureAirport.city} ({vuelo.departureAirport.code}) → {vuelo.arrivalAirport.city} ({vuelo.arrivalAirport.code})
-                        </div>
-                        <div>
-                            Salida: {new Date(vuelo.departureTime).toLocaleString()}
-                        </div>
-                        <div>
-                            Llegada: {new Date(vuelo.arrivalTime).toLocaleString()}
-                        </div>
-                        <div>
-                            Precio: {vuelo.price / 100} €
+            <div className="login-page">
+                <div id="clouds">
+                    <div className="cloud x1"></div>
+                    <div className="cloud x2"></div>
+                    <div className="cloud x3"></div>
+                    <div className="cloud x4"></div>
+                    <div className="cloud x5"></div>
+                    <div className="cloud x6"></div>
+                    <div className="cloud x7"></div>
+                </div>
+                <div className="resultado-vuelos-container">
+                    <h1>Confirmar Reserva</h1>
+                    <div className="card">
+                        <div className="info">
+                            <strong>{vuelo.airlineName}</strong>
+                            <div>
+                                {vuelo.departureAirport.city} ({vuelo.departureAirport.code}) → {vuelo.arrivalAirport.city} ({vuelo.arrivalAirport.code})
+                            </div>
+                            <div>
+                                Salida: {new Date(vuelo.departureTime).toLocaleString()}
+                            </div>
+                            <div>
+                                Llegada: {new Date(vuelo.arrivalTime).toLocaleString()}
+                            </div>
+                            <div>
+                                Precio: {vuelo.price / 100} €
+                            </div>
                         </div>
                     </div>
+                    <button className="btn" style={{ marginTop: "2rem" }}>Confirmar Reserva</button>
                 </div>
-                <button className="btn" style={{marginTop: "2rem"}}>Confirmar Reserva</button>
             </div>
         </Layout>
     );
